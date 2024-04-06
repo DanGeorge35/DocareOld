4e07d28969fd096a1a0866e894e1cfcdc7f8bcbb
Dan George <dangeorge35@gmail.com>
2024-04-06 02:57:30 +0100

    splashscreen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import 'react-native-pager-view';
 import 'react-native-gesture-handler';
 import 'react-native-reanimated';
 
-import React ,{useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import Splash_screen from './screens/Introscreens/Splash_screen';
 import Intro_screen from './screens/Introscreens/Intro_screen';
 import Lang_select from './screens/Introscreens/Lang_select';
@@ -26,7 +27,6 @@ import Location_info from './screens/Patient_profile/Location_info';
 import Communication_pref from './screens/Patient_profile/Communication_pref';
 import Security_settings from './screens/Patient_profile/Security_settings';
 
-
 //========================================================================
 
 import Medical_history from './screens/Patient_profile/Medical_info/Medical_history';
@@ -40,33 +40,27 @@ import Privacy_settings from './screens/Patient_profile/Communication_pref/Priva
 import Notification_settings from './screens/Patient_profile/Communication_pref/Notification_settings';
 import Terms from './screens/Patient_profile/Communication_pref/Terms';
 
-
 import Doctors_list_view from './screens/appointment_scheduling/Doctors_list_view';
 
+import {NativeBaseProvider, Text, Box} from 'native-base';
 
-
-
-import { NativeBaseProvider, Text, Box } from "native-base";
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
 export default function app() {
-
   const [count, setCount] = useState(5);
-  const [isSignUp , setIsSignUp] = useState(false);
-  const [isLoading , setIsLoading] = useState(true);
-
+  const [isSignUp, setIsSignUp] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const timer = setInterval(() => {
       if (count > 0) {
         setCount(count - 1);
-      }else{
-        setIsLoading(false)
-        setIsSignUp(false)
+      } else {
+        setIsLoading(false);
+        setIsSignUp(false);
       }
     }, 1000);
 
@@ -78,223 +72,202 @@ export default function app() {
   //   setIsLoading(false)
   // }
 
-
   if (isLoading) {
- 
     return <Splash_screen />;
-   
-  
- }
-
+  }
 
   return (
     <NativeBaseProvider>
-
-        <NavigationContainer>
-
+      <NavigationContainer>
         {/* <Stack.Navigator initialRouteName={isSignUp ? 'Intro_screen':'Login_screen'}> */}
 
-        <Stack.Navigator initialRouteName='Intro_screen' >
-     
-
-          <Stack.Group >
-          
-          
-            {/* <Stack.Screen 
-                name="Intro_screen" 
+        <Stack.Navigator initialRouteName="Intro_screen">
+          <Stack.Group>
+            {/* <Stack.Screen
+                name="Intro_screen"
                 options={{headerShown:false}}
-                component={Intro_screen} 
+                component={Intro_screen}
                 /> */}
 
-          <Stack.Screen 
-                name="Doctors_list_view" 
-                options={{headerShown:false}}
-                component={Doctors_list_view} 
-                />
-
-
+            <Stack.Screen
+              name="Doctors_list_view"
+              options={{headerShown: false}}
+              component={Doctors_list_view}
+            />
           </Stack.Group>
 
-         
-
-      <Stack.Screen 
-          name="Contact" 
-          options={{headerShown:false}}
-          component={Contact} 
+          <Stack.Screen
+            name="Contact"
+            options={{headerShown: false}}
+            component={Contact}
           />
 
-      <Stack.Screen 
-          name="Communication_pref" 
-          options={{headerShown:false}}
-          component={Communication_pref} 
+          <Stack.Screen
+            name="Communication_pref"
+            options={{headerShown: false}}
+            component={Communication_pref}
           />
 
-      <Stack.Screen 
-          name="Location_info" 
-          options={{headerShown:false}}
-          component={Location_info} 
+          <Stack.Screen
+            name="Location_info"
+            options={{headerShown: false}}
+            component={Location_info}
           />
 
-      <Stack.Screen 
-          name="Medical_info" 
-          options={{headerShown:false}}
-          component={Medical_info} 
+          <Stack.Screen
+            name="Medical_info"
+            options={{headerShown: false}}
+            component={Medical_info}
           />
 
-     <Stack.Screen 
-          name="Profile" 
-          options={{headerShown:false}}
-          component={Profile} 
+          <Stack.Screen
+            name="Profile"
+            options={{headerShown: false}}
+            component={Profile}
           />
 
-      <Stack.Screen 
-          name="Personal_info" 
-          options={{headerShown:false}}
-          component={Personal_info} 
+          <Stack.Screen
+            name="Personal_info"
+            options={{headerShown: false}}
+            component={Personal_info}
           />
 
-      <Stack.Screen 
-          name="Security_settings" 
-          options={{headerShown:false}}
-          component={Security_settings} 
+          <Stack.Screen
+            name="Security_settings"
+            options={{headerShown: false}}
+            component={Security_settings}
           />
 
-      <Stack.Screen 
-          name="Lang_select" 
-          options={{headerShown:false}}
-          component={Lang_select} 
+          <Stack.Screen
+            name="Lang_select"
+            options={{headerShown: false}}
+            component={Lang_select}
           />
 
-      <Stack.Screen 
-          name="Account_type" 
-          options={{headerShown:false}}
-          component={Account_type} 
+          <Stack.Screen
+            name="Account_type"
+            options={{headerShown: false}}
+            component={Account_type}
           />
 
-      <Stack.Screen 
-          name="Login_screen" 
-          options={{headerShown:false}}
-          component={Login_screen} 
+          <Stack.Screen
+            name="Login_screen"
+            options={{headerShown: false}}
+            component={Login_screen}
           />
 
-    <Stack.Screen 
-          name="Create_account" 
-          options={{headerShown:false}}
-          component={Create_account} 
+          <Stack.Screen
+            name="Create_account"
+            options={{headerShown: false}}
+            component={Create_account}
           />
 
-      <Stack.Screen 
-          name="Success_page" 
-          options={{headerShown:false}}
-          component={Success_page} 
+          <Stack.Screen
+            name="Success_page"
+            options={{headerShown: false}}
+            component={Success_page}
           />
 
-      <Stack.Screen 
-          name="Forget_pass" 
-          options={{headerShown:false}}
-          component={Forget_pass} 
+          <Stack.Screen
+            name="Forget_pass"
+            options={{headerShown: false}}
+            component={Forget_pass}
           />
 
-<Stack.Screen 
-          name="Retrieve_otp" 
-          options={{headerShown:false}}
-          component={Retrieve_otp} 
+          <Stack.Screen
+            name="Retrieve_otp"
+            options={{headerShown: false}}
+            component={Retrieve_otp}
           />
 
-<Stack.Screen 
-          name="Reset_password" 
-          options={{headerShown:false}}
-          component={Reset_password} 
+          <Stack.Screen
+            name="Reset_password"
+            options={{headerShown: false}}
+            component={Reset_password}
           />
 
-<Stack.Screen 
-          name="Successful_page" 
-          options={{headerShown:false}}
-          component={Successful_page} 
+          <Stack.Screen
+            name="Successful_page"
+            options={{headerShown: false}}
+            component={Successful_page}
           />
 
-  <Stack.Screen 
-          name="Current_medication" 
-          options={{headerShown:false}}
-          component={Current_medication} 
+          <Stack.Screen
+            name="Current_medication"
+            options={{headerShown: false}}
+            component={Current_medication}
           />
 
-<Stack.Screen 
-          name="Emergency_contact" 
-          options={{headerShown:false}}
-          component={Emergency_contact} 
+          <Stack.Screen
+            name="Emergency_contact"
+            options={{headerShown: false}}
+            component={Emergency_contact}
           />
 
-<Stack.Screen 
-          name="Chronic_conditions_mgt" 
-          options={{headerShown:false}}
-          component={Chronic_conditions_mgt} 
+          <Stack.Screen
+            name="Chronic_conditions_mgt"
+            options={{headerShown: false}}
+            component={Chronic_conditions_mgt}
           />
 
-<Stack.Screen 
-          name="Pry_care_phy_info" 
-          options={{headerShown:false}}
-          component={Pry_care_phy_info} 
+          <Stack.Screen
+            name="Pry_care_phy_info"
+            options={{headerShown: false}}
+            component={Pry_care_phy_info}
           />
 
-    <Stack.Screen 
-          name="Prefered_lang" 
-          options={{headerShown:false}}
-          component={Prefered_lang} 
+          <Stack.Screen
+            name="Prefered_lang"
+            options={{headerShown: false}}
+            component={Prefered_lang}
           />
 
-    <Stack.Screen 
-          name="Com_method" 
-          options={{headerShown:false}}
-          component={Com_method} 
+          <Stack.Screen
+            name="Com_method"
+            options={{headerShown: false}}
+            component={Com_method}
           />
 
-    <Stack.Screen 
-          name="Privacy_settings" 
-          options={{headerShown:false}}
-          component={Privacy_settings} 
+          <Stack.Screen
+            name="Privacy_settings"
+            options={{headerShown: false}}
+            component={Privacy_settings}
           />
 
-    <Stack.Screen 
-          name="Notification_settings" 
-          options={{headerShown:false}}
-          component={Notification_settings} 
+          <Stack.Screen
+            name="Notification_settings"
+            options={{headerShown: false}}
+            component={Notification_settings}
           />
 
-<Stack.Screen 
-          name="Terms" 
-          options={{headerShown:false}}
-          component={Terms} 
+          <Stack.Screen
+            name="Terms"
+            options={{headerShown: false}}
+            component={Terms}
           />
 
-<Stack.Screen 
-          name="Medical_history" 
-          options={{headerShown:false}}
-          component={Medical_history} 
+          <Stack.Screen
+            name="Medical_history"
+            options={{headerShown: false}}
+            component={Medical_history}
           />
+        </Stack.Navigator>
 
+        {/* <Splash_screen/> */}
+        {/* <Intro_screen/> */}
+        {/* <Lang_select/> */}
+        {/* <Account_type/> */}
+        {/* <Login_screen/> */}
+        {/* <Create_account/> */}
 
+        {/* <Success_page/> */}
+        {/* <Tabss/> */}
+        {/* <Forget_pass/> */}
+        {/* <Retrieve_otp/> */}
+        {/* <Reset_password/> */}
 
-
-
-  </Stack.Navigator>
-
-          {/* <Splash_screen/> */}
-          {/* <Intro_screen/> */}
-          {/* <Lang_select/> */}
-          {/* <Account_type/> */}
-          {/* <Login_screen/> */}
-          {/* <Create_account/> */}
-          
-          {/* <Success_page/> */}
-          {/* <Tabss/> */}
-          {/* <Forget_pass/> */}
-          {/* <Retrieve_otp/> */}
-          {/* <Reset_password/> */}
-
-          {/* <Successful_page/> */}
-          </NavigationContainer>
+        {/* <Successful_page/> */}
+      </NavigationContainer>
     </NativeBaseProvider>
-  )
+  );
 }
-
